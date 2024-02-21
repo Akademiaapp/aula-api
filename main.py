94c2b61914f831fc058eb6ef265c11bb9d599ef2
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-02-21 11:06:45 +0100

    remove unused code

--- main.py
+++ main.py
@@ -9,15 +9,6 @@ from unilogin import unilogin
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
 
 class User(BaseModel):
     username: str
@@ -31,6 +22,6 @@ def getCalenderEventsUsingUnilogin(user: User):
     
     return handler.getCalenderEvents()
 
-@app.post("/login")
-def login(user: User):
-    return {"username": user.username}
+# @app.post("/login")
+# def login(user: User):
+#     return {"username": user.username}
