616e47b54a7015e1f478c84129fe422ec988ecc4
Jonathan Bangert <jonathan@bangert.dk>
2024-03-08 13:05:24 +0000

    idk

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from aulaHandler import aulaHandler
 from unilogin import unilogin
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class User(BaseModel):
     username: str
