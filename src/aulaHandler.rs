da701763dcbc8aec0b2e9416be290c9c30ea97c9
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-10 23:16:04 +0100

    rust?

--- src/aulaHandler.rs
+++ src/aulaHandler.rs
@@ -1,5 +1,8 @@
 use std::fmt::Error;
-use reqwest::Client;
+use std::sync::Arc;
+use reqwest::{Client, Url};
+use reqwest::cookie::{CookieStore, Jar};
+use reqwest::header::HeaderValue;
 use crate::request_structs::get_events_by_profile_ids_and_resource_ids::GetEventsByProfileIdsAndResourceIdsReq;
 use crate::response_structs::get_events_by_profile_ids_and_resource_ids::{Daum, GetEventsByProfileIdsAndResourceIdsRes};
 use crate::response_structs::get_profiles_by_login::{Data, GetProfilesByLoginRes};
@@ -18,7 +21,7 @@ impl AulaSession {
         let session = unilogin(username, password).await.unwrap();
 
         //https://www.aula.dk/api/v18/?method=profiles.getProfilesByLogin
-        let profile_info = Self::request_profile_info(&session).await;
+        let profile_info = Self::request_profile_info(&session.client).await;
 
         let token = Self::get_cookie(&session, "Csrfp-Token".to_string());
         let php_session = Self::get_cookie(&session, "PHPSESSID".to_string());
@@ -30,6 +33,53 @@ impl AulaSession {
         Self {session, profile_info, token, php_session, id }
     }
 
+    pub async fn from_cookies(token: String, php_session: String) -> Self {
+        println!("hi");
+        // Create a cookie jar we can share with the HTTP client
+        let cookie_store  = reqwest_cookie_store::CookieStore::default();
+        let cookie_store = reqwest_cookie_store::CookieStoreMutex::new(cookie_store);
+        let cookie_store = std::sync::Arc::new(cookie_store);
+
+// create the HTTP client
+        let client = Client::builder()
+            .cookie_provider(Arc::clone(&cookie_store))
+            .cookie_store(true)
+            .build()
+            .unwrap();
+
+
+// Add some cookies
+//         let php_cookie =HeaderValue::from_static (
+//             format!("PHPSESSID={}; Domain=.aula.dk", php_session).as_str().clone()
+//         );
+//
+        let token_str = format!("Csrfp-Token={}; Domain=www.aula.dk", token);
+        let token_cookie = HeaderValue::from_str(&token_str).unwrap();
+
+        let php_str = format!("PHPSESSID={}; Domain=.aula.dk", php_session);
+        let php_cookie = HeaderValue::from_str(&token_str).unwrap();
+
+
+
+
+        cookie_store.set_cookies(&mut [&token_cookie].into_iter(), &Url::parse("https://www.aula.dk").unwrap());
+        cookie_store.set_cookies(&mut [&php_cookie].into_iter(), &Url::parse("https://www.aula.dk").unwrap());
+//
+//
+//
+//         let url = "https//www.aula.dk".parse::<Url>().unwrap();
+//         jar.set_cookies(&mut [&token_cookie].into_iter(), &url);
+//         jar.set_cookies(&mut [&php_cookie].into_iter(), &url);
+
+        let profile_info = Self::request_profile_info(&client).await;
+
+        println!("{:?}", profile_info);
+
+        let id = profile_info.profiles[0].institution_profiles[0].id;
+
+        Self {session: Session {client, cookie_store }, profile_info, token, php_session, id}
+    }
+
     fn get_cookie(session: &Session, name: String) -> String {
         let store = session.cookie_store.lock().unwrap();
         let x = store.iter_any().find(|c| c.name() == name).unwrap().value().to_string();
@@ -37,12 +87,16 @@ impl AulaSession {
     }
 
 
-    async fn request_profile_info(session: &Session) -> Data {
-        let r = session.client.get("https://www.aula.dk/api/v18/?method=profiles.getProfilesByLogin")
+    async fn request_profile_info(client: &Client) -> Data {
+        let r = client.get("https://www.aula.dk/api/v18/?method=profiles.getProfilesByLogin")
             .send()
-            .await;
+            .await
+            .unwrap();
 
-        let profile_info = r.unwrap().json::<GetProfilesByLoginRes>().await.unwrap();
+        let text = r.text().await.unwrap_or("".to_string());
+        println!("{:?}",  &text);
+
+        let profile_info = serde_json::from_str::<GetProfilesByLoginRes>(&text).unwrap();
 
         profile_info.data
     }
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .service(login)
+            .service(get_events)
 
     })
         .bind(("127.0.0.1", 8080))?
@@ -34,8 +35,8 @@ pub struct LoginRequest {
     password: String,
 }
 
-#[derive(Serialize)]
-pub struct LoginResponse {
+#[derive(Serialize, Deserialize)]
+pub struct LoginInfo {
     token: String,
     php_session: String,
 }
@@ -45,8 +46,24 @@ async fn login(info: Json<LoginRequest>) -> impl Responder {
     println!("hi");
     let aula_session = aulaHandler::AulaSession::new(&info.username, &info.password).await;
 
-    HttpResponse::Ok().json(LoginResponse { token: aula_session.token, php_session: aula_session.php_session })
+    HttpResponse::Ok().json(LoginInfo { token: aula_session.token, php_session: aula_session.php_session })
 }
+
+#[derive(Deserialize)]
+struct EventRequest {
+    login_info: LoginInfo,
+    start: String,
+    end: String
+}
+
+#[post("/getCalenderEvents")]
+async fn get_events(info: Json<EventRequest>) -> impl Responder {
+    let aula_session = aulaHandler::AulaSession::from_cookies(info.login_info.token.clone(), info.login_info.php_session.clone()).await;
+    let events = aula_session.request_events(info.start.to_string(), info.end.to_string()).await.unwrap();
+    HttpResponse::Ok().json(events)
+}
+
+
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
