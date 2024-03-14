3c8d25d94f1ba85ba9a781578d5b91ce35cd2a8f
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-14 20:15:55 +0100

    remove debugging code

--- src/main.rs
+++ src/main.rs
@@ -66,15 +66,11 @@ async fn get_events(info: Json<EventRequest>) -> impl Responder {
 
 #[get("/getNotifications")]
 async fn get_notifs(info: Json<LoginInfo>) -> impl Responder {
-    let start = Instant::now();
+
     let aula_session = AulaSession::from_login_info(&info.into_inner()).await;
 
     let url = format!("https://www.aula.dk/api/v18/?method=notifications.getNotificationsForActiveProfile&activeInstitutionCodes[]={}", aula_session.institution_code);
 
-    println!(
-        "Time elapsed in expensive_function() is: {:?}",
-        start.elapsed()
-    );
     let res = aula_session.request_get(url).await.unwrap();
 
     HttpResponse::Ok().json(res)
